f41ac9721e1703e7a5ffc6dd4b4bfb17955d01d3
Rob Wymer <wymer1120@gmail.com>
2017-07-10 13:24:39 -0400

    First Commit

---
+++ ScrollView Image Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView Image Gallery
+//
+//  Created by Alex Wymer  on 2017-07-10.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollView Image Gallery/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollView Image Gallery
+//
+//  Created by Alex Wymer  on 2017-07-10.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
